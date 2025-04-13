6b43ae0c6efb14c86a1c13e79908d66310dad531
mostofareza3 <mostofa.reza3@gmail.com>
2025-04-13 07:02:13 +0600

    refector code using event emitter

--- understanding-file-system/project/app.js
+++ understanding-file-system/project/app.js
@@ -4,12 +4,26 @@ import fs from "fs/promises"
 
 (async () =>{
     const watcher = fs.watch("./command.txt")
-    const commandFile = await fs.open('./command.txt')
+    const commandFile = await fs.open('./command.txt', "r")
+
+    commandFile.on("change", async()=>{
+        // get size of the file
+        const size = (await commandFile.stat()).size
+        // allocate buffer with the size of file
+        const buff = Buffer.alloc(size)
+        // the position we want to start reading
+        const position = 0;
+        // the location at which we want to start filling our buffer
+        const offset = 0;
+        // how many bytes we want to read
+        const length = buff.byteLength;
+        const content = await commandFile.read(buff, offset, length, position)
+        console.log(content)
+    })
 
     for await (const event of watcher){
         if(event.eventType === "change"){
-            const content = await commandFile.read()
-            console.log(content)
+            commandFile.emit("change")
         }
     }
 })()
