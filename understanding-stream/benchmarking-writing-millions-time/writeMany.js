122973aca8b4a8b40dad8851d5baaff070cf8adc
mostofareza3 <mostofa.reza3@gmail.com>
2025-04-13 23:39:44 +0600

    introduction to stream

--- understanding-stream/benchmarking-writing-millions-time/writeMany.js
+++ understanding-stream/benchmarking-writing-millions-time/writeMany.js
@@ -39,15 +39,38 @@ import fs from "fs/promises";
 // Execution time: 131.857ms
 // CPU Usage: 
 // Memory Uses: 200MB
+// (async()=>{
+//     console.time("writeMany")
+//     const fileHandle = await fs.open("text.txt", "w");
+
+//     const stream = fileHandle.createWriteStream()
+
+//     for(let i=0; i <10000000 ; i++){
+//         const buff = Buffer.from(` ${i} `, "utf-8")
+//         stream.write(buff)
+//     }
+//     console.timeEnd("writeMany")
+// })()
+
+
+
 (async()=>{
     console.time("writeMany")
     const fileHandle = await fs.open("text.txt", "w");
 
     const stream = fileHandle.createWriteStream()
 
-    for(let i=0; i <10000000 ; i++){
-        const buff = Buffer.from(` ${i} `, "utf-8")
-        stream.write(buff)
-    }
+    const buff = Buffer.alloc(stream.writableHighWaterMark, 10) 
+    console.log(stream.write(buff))
+    console.log(stream.write(Buffer.alloc(1,"a")))
+
+
+    stream.on("drain", ()=>{
+        console.log("we are ready to write another stream")
+    })
+
+    // stream.write(buff)
+    // console.log(stream.writableLength)
+
     console.timeEnd("writeMany")
 })()
