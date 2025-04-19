549a6f7adef13cf213add5a75b94991a93b53d36
mostofareza3 <mostofa.reza3@gmail.com>
2025-04-19 22:44:43 +0600

    fix: make the console something cleaner

--- understanding-networking/chat-app/client.js
+++ understanding-networking/chat-app/client.js
@@ -6,20 +6,50 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve()
+        })
+    })
+}
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve()
+        })
+    })
+}
+
 const socket = net.createConnection(
     { port: 3333, host: "127.0.0.1" },
     async () => {
         console.log("connected to the server!");
 
-        const message = await rl.question("Enter a message:>> ");
-        socket.write(message);
-        rl.close();
+        const ask = async () => {
+            const message = await rl.question("Enter a message:>> ");
+            await moveCursor(0, -1)
+            await clearLine(0)
+            socket.write(message);
+        }
+        ask()
+
+
+        socket.on("data", async (data) => {
+            console.log("called")
+            //empty line
+            console.log()
+            // move cursor one line up
+            await moveCursor(0, -1)
+            // clear line
+            await clearLine(0)
+            console.log(data.toString("utf-8"))
+            ask()
+        })
     }
 );
 
-socket.on("data", (data)=>{
-    console.log(data.toString("utf-8"))
-})
 
 socket.on("end", () => {
     console.log("Connection was ended!");
