a4fb6332b4eea7c16765cb8e86d1d15e61a8e4fa
mostofareza3 <mostofa.reza3@gmail.com>
2025-04-23 09:29:05 +0600

    feature: add UDP message sending functionality to simple sender implementation

--- understanding-networking/simple-udp/simple-sender.js
+++ understanding-networking/simple-udp/simple-sender.js
@@ -1,3 +1,8 @@
 import dgram from "dgram"
 
 const sender = dgram.createSocket('udp6')
+
+sender.send("This is a string", 8000, "::1", (error, bytes)=>{
+    if(error) console.log(error)
+    console.log(bytes)
+})
