0480104f7b7770d7a652ef633edfc6e2cc9f0f3f
mostofareza3 <mostofa.reza3@gmail.com>
2026-02-14 20:09:20 +0600

    Understanding CPU heavy operation

--- understanding-multithreding/threads/prime-generator/app.js
+++ understanding-multithreding/threads/prime-generator/app.js
@@ -1,6 +1,45 @@
 const { Worker } = require("worker_threads");
 const { performance } = require("perf_hooks");
 
+const start = performance.now();
+
+
+console.log(`Time taken: ${performance.now() - start}ms`)
+
+let result = []
+const THREADS = 4;
+let completed = 0;
+const count = 100; // number of prime numbers that we want
+for (let i = 0; i < THREADS; i++) {
+  const worker = new Worker("./calc.js", {
+    workerData: { count: count / THREADS, start: 100_000_000_000_000 + i * 300 }
+  })
+  const threadId = worker.threadId;
+  console.log(`Worker ${threadId} started!`)
+  worker.on("message", (primes) => {
+    result = result.concat(primes)
+  })
+  worker.on("error", (err) => {
+    console.log(err)
+  })
+  worker.on("exit", (code) => {
+    console.log(`Worker ${threadId} exited!`)
+    completed++;
+
+    if (completed === THREADS) {
+      console.log(`Time taken: ${performance.now() - start}ms`)
+      console.log(result.sort())
+    }
+
+    if (code !== 0) {
+      console.log(`Worker ${threadId} exited with code {code}`)
+    }
+
+  })
+}
+
+
+
 // count: 200, start: 100_000_000_000_000, time: 30s, threads=1, numbers
 // count: 200, start: 100_000_000_000_000, time: 8s, threads=4, numbers
 
@@ -8,42 +47,42 @@ const { performance } = require("perf_hooks");
 // count: 80, start: 12n ** 17n, time: 82s, threads=1, Bigints
 // count: 80, start: 12n ** 17n, time: 20s, threads=8, Bigints
 
-let result = [];
-const THREADS = 4;
-let completed = 0;
-const count = 100; // number of prime numbers that we want
-const start = performance.now();
+// let result = [];
+// const THREADS = 4;
+// let completed = 0;
+// const count = 100; // number of prime numbers that we want
+// const start = performance.now();
 
-for (let i = 0; i < THREADS; i++) {
-  const worker = new Worker("./calc.js", {
-    workerData: {
-      count: count / THREADS,
-      start: 100_000_000_000_000 + i * 300,
-    },
-  });
-  const threadId = worker.threadId;
-  console.log(`Worker ${threadId} started.`);
+// for (let i = 0; i < THREADS; i++) {
+//   const worker = new Worker("./calc.js", {
+//     workerData: {
+//       count: count / THREADS,
+//       start: 100_000_000_000_000 + i * 300,
+//     },
+//   });
+//   const threadId = worker.threadId;
+//   console.log(`Worker ${threadId} started.`);
 
-  worker.on("message", (primes) => {
-    result = result.concat(primes);
-  });
+//   worker.on("message", (primes) => {
+//     result = result.concat(primes);
+//   });
 
-  worker.on("error", (err) => {
-    console.error(err);
-  });
+//   worker.on("error", (err) => {
+//     console.error(err);
+//   });
 
-  worker.on("exit", (code) => {
-    console.log(`Worker ${threadId} exited.`);
+//   worker.on("exit", (code) => {
+//     console.log(`Worker ${threadId} exited.`);
 
-    completed++;
+//     completed++;
 
-    if (completed === THREADS) {
-      console.log(`Time Taken: ${performance.now() - start}ms`);
-      console.log(result.sort());
-    }
+//     if (completed === THREADS) {
+//       console.log(`Time Taken: ${performance.now() - start}ms`);
+//       console.log(result.sort());
+//     }
 
-    if (code !== 0) {
-      console.error(`Worker ${threadId} exited with code ${code}`);
-    }
-  });
-}
+//     if (code !== 0) {
+//       console.error(`Worker ${threadId} exited with code ${code}`);
+//     }
+//   });
+// }
--- understanding-multithreding/threads/prime-generator/calc.js
+++ understanding-multithreding/threads/prime-generator/calc.js
@@ -1,7 +1,11 @@
 const { workerData, parentPort } = require("worker_threads");
 const generatePrimes = require("./prime-generator");
 
-const primes = generatePrimes(workerData.count, workerData.start, {
-  format: true,
-});
-parentPort.postMessage(primes);
+
+const primes = generatePrimes(workerData.count, workerData.start)
+parentPort.postMessage(primes)
+
+// const primes = generatePrimes(workerData.count, workerData.start, {
+//   format: true,
+// }); 
+// parentPort.postMessage(primes);
